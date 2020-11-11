c9fd358dae396e3e38fae55b5f0893d03398315a
uchiyama777 <ucchi_uchi@hotmail.co.jp>
2020-11-11 19:27:25 +0900

    ユーザー管理機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'artists/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "artists#index"
+  resources :users, only: [:edit]
 end
